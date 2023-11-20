d35528e9f14abfaa5f7b9cfa34356f3536628910
anabeldilab <alu0101206011@ull.edu.es>
2023-11-20 16:29:26 +0000

    Feat: Add basic file structure and Heltec library

---
+++ minigames/minigames.ino
@@ -0,0 +1,18 @@
+/** Universidad de La Laguna
+  * Escuela de Doctorado y Estudios de Postgrado
+  * Máster Universitario en Informática Industrial y Robótica
+  * Lenguajes de Alto Nivel para Aplicaciones Industriales
+  *
+  * @author Anabel Díaz Labrador <alu0101206011@ull.edu.es>
+  * @date 20 Nov 2023
+  * @brief This code contains the implementation of different minigames, such as snake or tetris. 
+  *       The user can choose which game to play. 
+*/
+
+#include "heltec.h"
+
+void setup() {
+}
+
+void loop() {
+}

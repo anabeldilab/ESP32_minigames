3c2cc49b1f94aed5705ca6fc1395d4a7b3a27792
anabeldilab <alu0101206011@ull.edu.es>
2023-11-20 22:57:01 +0000

    Refactor: Add spaces between methods

--- minigames/src/position.cpp
+++ minigames/src/position.cpp
@@ -2,31 +2,41 @@
 
 Position::Position(): x_(1), y_(1) {}
 
+
 Position::Position(int x, int y): x_(x), y_(y) {}
+
   
 int Position::get_x(void) const { 
   return x_; 
 }
+
+
 int Position::get_y(void) const { 
   return y_; 
 }
 
+
 void Position::set_x(const int& kNewX) {
   x_ = kNewX;
 }
+
+
 void Position::set_y(const int& kNewY) {
   y_ = kNewY;
 }
+
 
 bool Position::operator==(const Position& kOtherPosition) {
   return ((x_ == kOtherPosition.get_x()) && 
            y_ == kOtherPosition.get_y());
 }
 
+
 bool Position::operator!=(const Position& kOtherPosition) {
   return ((x_ != kOtherPosition.get_x()) || 
            y_ != kOtherPosition.get_y());
 }
+
 
 Position Position::operator+(const Position& kOtherPosition) {
   Position result;
